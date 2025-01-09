d079c239f5bb72a312f6609b879b0c4d40188be6
Nataliia Khinchuk <evettakh@gmail.com>
2025-01-09 15:06:55 +0200

    update numbers.length task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
   const newArray = [];
-  for (let i = 0; i <= numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
       newArray.push(numbers[i]);
     }
